6a01c7befc849af0a8b08762bd890fdc70774db4
arshedgithub <arshedahmedmail@gmail.com>
2024-06-18 17:16:10 +0530

    add button click event

---
+++ app/src/main/java/com/example/mad1/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.mad1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button btn_first = findViewById(R.id.btn_first);
+        TextView txt_view = findViewById(R.id.txt_welcome);
+
+        btn_first.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txt_view.setText("Button Clicked !");
+            }
+        });
+    }
+}
