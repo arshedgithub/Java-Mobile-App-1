94c88fc9278a7c9737efc085f6d0faa9f65c12f4
arshedgithub <arshedahmedmail@gmail.com>
2024-06-24 02:45:09 +0530

    get sensor data

--- app/src/main/java/com/example/mad1/Page01.java
+++ app/src/main/java/com/example/mad1/Page01.java
@@ -1,9 +1,14 @@
 package com.example.mad1;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,12 +20,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Page01 extends AppCompatActivity {
+public class Page01 extends AppCompatActivity implements SensorEventListener {
 
     Button btn_home;
     Button btn_second;
     Button btn_third;
-    TextView txtContentPg1;
+    TextView txtXPg1;
+    TextView txtYPg1;
+    TextView txtZPg1;
+
+    private SensorManager sensorManager;
+    private Sensor accelorometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +41,13 @@ public class Page01 extends AppCompatActivity {
         btn_home = findViewById(R.id.btn_pg_1_home);
         btn_second = findViewById(R.id.btn_pg_1_2);
         btn_third = findViewById(R.id.btn_pg_1_3);
-        txtContentPg1 = findViewById(R.id.txt_content_pg_1);
+        txtXPg1 = findViewById(R.id.txtx_content_pg_1);
+        txtYPg1 = findViewById(R.id.txty_content_pg_1);
+        txtZPg1 = findViewById(R.id.txtz_content_pg_1);
 
-        // String msg = getIntent().getStringExtra("message");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelorometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener((SensorEventListener) this, accelorometer, SensorManager.SENSOR_DELAY_NORMAL);
 
         DatabaseHelper dbhelper = new DatabaseHelper(this);
         SQLiteDatabase database = dbhelper.getReadableDatabase();
@@ -43,7 +57,7 @@ public class Page01 extends AppCompatActivity {
         if (cursor.moveToNext()){
             String name = cursor.getString(1);
             String value = cursor.getString(2);
-            txtContentPg1.setText("Name : " + name + "\nValue : " + value);
+            txtXPg1.setText("Name : " + name + "\nValue : " + value);
         }
         cursor.close();
 
@@ -72,4 +86,14 @@ public class Page01 extends AppCompatActivity {
             public void onClick(View v) { startActivity(new Intent(Page01.this, Page03.class)); }
         });
     }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        txtXPg1.setText("X Axis: " + event.values[0]);
+        txtYPg1.setText("Y Axis: " + event.values[1]);
+        txtZPg1.setText("Z Axis: " + event.values[2]);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 }
