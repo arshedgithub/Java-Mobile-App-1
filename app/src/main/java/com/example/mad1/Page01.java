fdc89fdffd360af82756c17c4cc321522cfddc85
arshedgithub <arshedahmedmail@gmail.com>
2024-06-24 10:34:53 +0530

    color according to accelorometer value

--- app/src/main/java/com/example/mad1/Page01.java
+++ app/src/main/java/com/example/mad1/Page01.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,6 +27,8 @@ public class Page01 extends AppCompatActivity implements SensorEventListener {
     Button btn_home;
     Button btn_second;
     Button btn_third;
+    TextView txtHeader;
+    TextView txtUserPg1;
     TextView txtXPg1;
     TextView txtYPg1;
     TextView txtZPg1;
@@ -41,6 +45,8 @@ public class Page01 extends AppCompatActivity implements SensorEventListener {
         btn_home = findViewById(R.id.btn_pg_1_home);
         btn_second = findViewById(R.id.btn_pg_1_2);
         btn_third = findViewById(R.id.btn_pg_1_3);
+        txtHeader = findViewById(R.id.page_1_head);
+        txtUserPg1 = findViewById(R.id.txtuser_content_pg_1);
         txtXPg1 = findViewById(R.id.txtx_content_pg_1);
         txtYPg1 = findViewById(R.id.txty_content_pg_1);
         txtZPg1 = findViewById(R.id.txtz_content_pg_1);
@@ -57,13 +63,13 @@ public class Page01 extends AppCompatActivity implements SensorEventListener {
         if (cursor.moveToNext()){
             String name = cursor.getString(1);
             String value = cursor.getString(2);
-            txtXPg1.setText("Name : " + name + "\nValue : " + value);
+            txtUserPg1.setText("Name : " + name + "\nValue : " + value);
         }
         cursor.close();
-
+//
 //        SharedPreferences sharedPreferences = getSharedPreferences("userSettings", MODE_PRIVATE);
 //        String msg = sharedPreferences.getString("message", "Shared Preference not found");
-//        txtContentPg1.setText(msg);
+//        txtUserPg1.setText(msg);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -89,9 +95,23 @@ public class Page01 extends AppCompatActivity implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        txtXPg1.setText("X Axis: " + event.values[0]);
-        txtYPg1.setText("Y Axis: " + event.values[1]);
-        txtZPg1.setText("Z Axis: " + event.values[2]);
+        switch (event.sensor.getType()){
+            case Sensor.TYPE_ACCELEROMETER: acceleroMeterFunc(event); break;
+        }
+    }
+
+    private void acceleroMeterFunc(SensorEvent event) {
+        float x = event.values[0];
+        float y = event.values[1];
+        float z = event.values[2];
+        txtXPg1.setText("X Axis: " + x);
+        txtYPg1.setText("Y Axis: " + y);
+        txtZPg1.setText("Z Axis: " + z);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            txtHeader.setBackgroundColor(Color.rgb(x, y, z));
+//            getWindow().getDecorView().setBackgroundColor(Color.rgb(x, y, z));
+        }
     }
 
     @Override
